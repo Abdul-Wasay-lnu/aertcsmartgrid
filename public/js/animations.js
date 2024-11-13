71df0a06554339e86110ff4dee34e250f6126345
Urooj Alam <urooj-alam@outlook.com>
2024-11-13 02:09:56 -0500

    labview dashboard

---
+++ public/js/animations.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var homeAnim = lottie.loadAnimation({
+      container: document.getElementById('home'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animations/Home.json'
+    });
+  
+    var gridAnim = lottie.loadAnimation({
+      container: document.getElementById('powerGrid'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animations/grid.json'
+    });
+  
+   // Array to hold battery animation instances
+   var batteryAnims = [];
+
+   // Initialize all battery animations
+   for (let i = 1; i <= 6; i++) {
+     batteryAnims.push(lottie.loadAnimation({
+       container: document.getElementById(`battery-${i}`),
+       renderer: 'svg',
+       loop: true,
+       autoplay: true,
+       path: 'assets/animations/battery.json' // Common battery animation file
+     }));
+   }
+
+   // Function to update battery animation based on percentage
+   function updateBatteryAnimation(batteryIndex, percentage) {
+    
+     const animation = batteryAnims[batteryIndex];
+
+     // Update battery percentage - assuming the Lottie animation has frames representing different charging levels
+     // E.g., from 0% to 100%, spread over 0 to 100 frames
+     const frame = Math.round((percentage / 100) * animation.totalFrames); // Total frames based on battery percentage
+     console.log('Frame:', frame);
+     if(percentage==100){
+      animation.goToAndStop(frame-1, true); // Go to the frame corresponding to the battery percentage
+
+    }else{
+     animation.goToAndStop(frame, true); // Go to the frame corresponding to the battery percentage
+    }
+     // Update battery color dynamically (you can change the logic based on specific requirements)
+     const batteryElement = document.getElementById(`battery-${batteryIndex + 1}`);
+   // Update the displayed value for the battery
+  const batteryValueElement = document.getElementById(`battery-value-${batteryIndex + 1}`);
+  batteryValueElement.textContent = `${percentage} V`; // Update the value text
+   }
+
+   // Fetch data from the server and update the animations
+   function fetchDataAndUpdate() {
+     fetch('/get-latest-data')
+       .then(response => response.json())
+       .then(data => {
+         console.log('Fetched data:', data);
+
+         // Assuming you have battery data in the format { variable: "Battery 1", value: 23 }
+         data.forEach(item => {
+          //  if (item.variable.startsWith('Battery')) {
+          //    const batteryIndex = parseInt(item.variable.split(' ')[1]); // Extract battery number
+          //    console.log('Battery index:', batteryIndex);
+          //    updateBatteryAnimation(batteryIndex, item.value); // Update battery animation
+          //  }
+while(item.BatteryIndex<=6){
+           console.log('Battery index:', item.BatteryIndex);
+           updateBatteryAnimation(item.BatteryIndex, item.value); // Update battery animation
+           break;
+         }});
+       })
+       .catch(error => console.error('Error fetching data:', error));
+   }
+
+   // Fetch and update the battery every 3 seconds
+   setInterval(fetchDataAndUpdate, 3000);
+   //fetchDataAndUpdate();
+    var invertersAnim = lottie.loadAnimation({
+      container: document.getElementById('inverters'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animations/capacitor.json'
+ 
+    });
+
+    var motorAnim = lottie.loadAnimation({
+      container: document.getElementById('motor'),
+      renderer: 'mp4',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animations/motor-animation.mp4'
+    });
+  });
+  
---
+++ server.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const mysql = require('mysql2');
+app.use(bodyParser.urlencoded({
+    extended: true
+    }));
+// Create a MySQL connection
+const connection = mysql.createConnection({
+    host: 'smartgrid-server',
+    user: 'mqvjbukjvl',
+    password: '$87quhc3uRXK4rVk',
+    database: 'smartgrid-database'
+});
+
+// Create a table if it doesn't exist
+connection.query(
+    `CREATE TABLE IF NOT EXISTS battery_data (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        BatteryIndex INT(255),
+        value DECIMAL(10, 2),
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`, (err, results) => {
+        if (err) throw err;
+        console.log("Table created or already exists");
+    }
+);
+
+// Serve static HTML file
+app.use(express.static('public'));
+
+// Use body-parser to parse incoming JSON
+app.use(bodyParser.json());
+app.use(express.text());
+app.use(express.json());
+
+// Endpoint to handle HTTP POST from LabVIEW
+app.post('/receive-data', (req, res) => {
+    const labviewData = req.body; // Store the received data
+    console.log('Data received:', labviewData);
+    
+    // Use a Promise to handle database insertion
+    const insertPromises = Object.entries(labviewData).map(([key, value]) => {
+        return new Promise((resolve, reject) => {
+            const query = `INSERT INTO battery_data (BatteryIndex, value) VALUES (?, ?)`;
+            connection.query(query, [key, value], (err, result) => {
+                if (err) return reject(err); // Reject the promise on error
+                // console.log('Data inserted:', result);
+                resolve(result);
+            });
+        });
+    });
+
+    // Wait for all insert operations to complete
+    Promise.all(insertPromises)
+        .then(() => {
+            res.json({ message: 'Data saved to database', data: labviewData });
+        })
+        .catch(err => {
+            console.error('Error inserting data:', err);
+            res.status(500).json({ message: 'Error saving data to database' });
+        });
+});
+
+// Endpoint to get the latest data
+app.get('/get-latest-data', (req, res) => {
+    const query = `SELECT BatteryIndex, value, timestamp FROM battery_data ORDER BY id DESC LIMIT 5`;
+    connection.query(query, (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Error fetching data' });
+        } else {
+            
+            results = results.map(row => ({
+                BatteryIndex: row.BatteryIndex,
+                value: row.value,
+                timestamp: row.timestamp.toISOString() // Convert to a string
+            }));
+            console.log('Results from database:', results); // Log the results
+            res.json(results); // Return the latest record
+          
+        }
+    });
+});
+
+// Endpoint to get the latest data for a specific sensor
+app.get('/get-history/:sensor', (req, res) => {
+    const sensor = req.params.sensor;
+    const query = `SELECT BatteryIndex, value, timestamp FROM battery_data WHERE BatteryIndex = ? ORDER BY timestamp DESC`;
+
+    connection.query(query, [sensor], (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Error fetching data' });
+        } else {
+            console.log('Results from database for history of sensor:', results); // Log the results
+            
+            res.json(results); // Return all records for the specified sensor
+        }
+    });
+});
+
+// Endpoint to get all historical data
+app.get('/get-all-history', (req, res) => {
+    const query = `SELECT BatteryIndex, value, timestamp FROM battery_data ORDER BY timestamp DESC`;
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Error fetching data' });
+        } else {
+            console.log('Results from database for all history:', results); // Log the results
+            res.json(results); // Return all historical records
+        }
+    });
+});
+
+const value = new Array(2);
+let buttonstate = 0;
+// GET endpoint to retrieve data based on two keys to be used by checkbox buttons
+app.post('/control/', (req, res) => {
+    console.log(req.body); 
+    // const btid = req.body.btid;
+    const cn = req.body.cn;
+    // var receiveddata = req.body; 
+    value[0]=btid;
+    value[1]=cn;
+    console.log(value);
+    // Send a response
+    res.json({ message: 'keys received ', data: value });
+});
+
+// Endpoint to get the latest data
+app.post('/get-btcontrol', (req, res) => {
+    // value[1]=buttonstate;
+    // console.log("in get",value);
+    // if(value[0]>= 0 || value[1]>= 0 )
+    // {
+    // res.json(value); // Return the latest record
+    // } 
+    // buttonstate = 0;
+    res.json(parseInt(buttonstate, 10));
+    console.log("in getbt", buttonstate);
+});
+
+app.post('/update-radio', (req, res) => {
+    const radioState = req.body;
+    buttonstate = radioState;
+    console.log('Received button state:',buttonstate);
+    // res.json({ message: 'Radio state received', buttonstate });
+    res.json({ message: 'Power button state', buttonstate: parseInt(buttonstate, 10) });
+  });
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
